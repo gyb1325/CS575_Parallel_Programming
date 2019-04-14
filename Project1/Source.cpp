9e838d785ae5e5601fdc4a6c6948f1ec16f6167e
gyb1325 <guyongbin1325@gmail.com>
2019-04-14 15:03:52 -0700

    complete project1

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -6,18 +6,18 @@
 
 // setting the number of threads:
 #ifndef NUMT
-#define NUMT		5
+#define NUMT		2
 #endif
-#define NUMTRIALS	10000000
+/*
+#define NUMTRIALS	10000
 // setting the number of trials in the monte carlo simulation:
 #ifndef NUMTRIALS
 #define NUMTRIALS	1000000
-
 #endif
-
+*/
 // how many tries to discover the maximum performance:
 #ifndef NUMTRIES
-#define NUMTRIES	10
+#define NUMTRIES	50
 #endif
 
 // ranges for the random numbers:
@@ -42,106 +42,113 @@ main(int argc, char *argv[])
 	return 1;
 #endif
 
-	TimeOfDaySeed();		// seed the random number generator
+	//TimeOfDaySeed();		// seed the random number generator
+	for (int NUMTRIALS = 10; NUMTRIALS <= 1000000; NUMTRIALS *= 10) {
+		for (int i = 1; i <= 8; i = i * 2) {
 
-	omp_set_num_threads(NUMT);	// set the number of threads to use in the for-loop:`
+			omp_set_num_threads(i);	// set the number of threads to use in the for-loop:`
 
-	// better to define these here so that the rand() calls don't get into the thread timing:
-	float *xcs = new float[NUMTRIALS];
-	float *ycs = new float[NUMTRIALS];
-	float * rs = new float[NUMTRIALS];
+			// better to define these here so that the rand() calls don't get into the thread timing:
+			float *xcs = new float[NUMTRIALS];
+			float *ycs = new float[NUMTRIALS];
+			float * rs = new float[NUMTRIALS];
 
-	// fill the random-value arrays:
-	for (int n = 0; n < NUMTRIALS; n++)
-	{
-		xcs[n] = Ranf(XCMIN, XCMAX);
-		ycs[n] = Ranf(YCMIN, YCMAX);
-		rs[n] = Ranf(RMIN, RMAX);
-	}
+			// fill the random-value arrays:
+			for (int n = 0; n < NUMTRIALS; n++)
+			{
+				xcs[n] = Ranf(XCMIN, XCMAX);
+				ycs[n] = Ranf(YCMIN, YCMAX);
+				rs[n] = Ranf(RMIN, RMAX);
+			}
 
-	// get ready to record the maximum performance and the probability:
-	float maxPerformance = 0.;      // must be declared outside the NUMTRIES loop
-	float currentProb;              // must be declared outside the NUMTRIES loop
+			// get ready to record the maximum performance and the probability:
+			float maxPerformance = 0.;      // must be declared outside the NUMTRIES loop
+			float currentProb;              // must be declared outside the NUMTRIES loop
 
-									// looking for the maximum performance:
-	for (int t = 0; t < NUMTRIES; t++)
-	{
-		double time0 = omp_get_wtime();
+											// looking for the maximum performance:
+			for (int t = 0; t < NUMTRIES; t++)
+			{
+				double time0 = omp_get_wtime();
 
-		int numHits = 0;
+				int numHits = 0;
 #pragma omp parallel for default(none) shared(xcs,ycs,rs) reduction(+:numHits)
-		for (int n = 0; n < NUMTRIALS; n++)
-		{
-			// randomize the location and radius of the circle:
-			float xc = xcs[n];
-			float yc = ycs[n];
-			float  r = rs[n];
+				for (int n = 0; n < NUMTRIALS; n++)
+				{
+					// randomize the location and radius of the circle:
+					float xc = xcs[n];
+					float yc = ycs[n];
+					float  r = rs[n];
 
-			// solve for the intersection using the quadratic formula:
-			float a = 2.;
-			float b = -2.*(xc + yc);
-			float c = xc * xc + yc * yc - r * r;
-			float d = b * b - 4.*a*c;
-			//Case A
-			if (d < 0.0) continue;
+					// solve for the intersection using the quadratic formula:
+					float a = 2.;
+					float b = -2.*(xc + yc);
+					float c = xc * xc + yc * yc - r * r;
+					float d = b * b - 4.*a*c;
+					//Case A
+					if (d < 0.0) continue;
 
-			// hits the circle:
-			// get the first intersection:
-			d = sqrt(d);
-			float t1 = (-b + d) / (2.*a);	// time to intersect the circle
-			float t2 = (-b - d) / (2.*a);	// time to intersect the circle
-			float tmin = t1 < t2 ? t1 : t2;		// only care about the first intersection
-			//Case B 
-			if (tmin < 0.0) continue;
-
-
+					// hits the circle:
+					// get the first intersection:
+					d = sqrt(d);
+					float t1 = (-b + d) / (2.*a);	// time to intersect the circle
+					float t2 = (-b - d) / (2.*a);	// time to intersect the circle
+					float tmin = t1 < t2 ? t1 : t2;		// only care about the first intersection
+					//Case B 
+					if (tmin < 0.0) continue;
 
 
-				// where does it intersect the circle?
-			float xcir = tmin;
-			float ycir = tmin;
-
-			// get the unitized normal vector at the point of intersection:
-			float nx = xcir - xc;
-			float ny = ycir - yc;
-			float n_dis = sqrt(nx*nx + ny * ny);
-			nx /= n_dis;	// unit vector
-			ny /= n_dis;	// unit vector
-
-						// get the unitized incoming vector:
-			float inx = xcir - 0.;
-			float iny = ycir - 0.;
-			float in = sqrt(inx*inx + iny * iny);
-			inx /= in;	// unit vector
-			iny /= in;	// unit vector
-
-						// get the outgoing (bounced) vector:
-			float dot = inx * nx + iny * ny;
-			float outx = inx - 2.*nx*dot;	// angle of reflection = angle of incidence`
-			float outy = iny - 2.*ny*dot;	// angle of reflection = angle of incidence`
-
-											// find out if it hits the infinite plate:
-			float t = (0. - ycir) / outy;
-			
-			if (t < 0.0) continue;
-			
-			numHits++;
 
 
+					// where does it intersect the circle?
+					float xcir = tmin;
+					float ycir = tmin;
+
+					// get the unitized normal vector at the point of intersection:
+					float nx = xcir - xc;
+					float ny = ycir - yc;
+					float n_dis = sqrt(nx*nx + ny * ny);
+					nx /= n_dis;	// unit vector
+					ny /= n_dis;	// unit vector
+
+								// get the unitized incoming vector:
+					float inx = xcir - 0.;
+					float iny = ycir - 0.;
+					float in = sqrt(inx*inx + iny * iny);
+					inx /= in;	// unit vector
+					iny /= in;	// unit vector
+
+								// get the outgoing (bounced) vector:
+					float dot = inx * nx + iny * ny;
+					float outx = inx - 2.*nx*dot;	// angle of reflection = angle of incidence`
+					float outy = iny - 2.*ny*dot;	// angle of reflection = angle of incidence`
+
+													// find out if it hits the infinite plate:
+					float t = (0. - ycir) / outy;
+
+					if (t < 0.0) continue;
+
+					numHits++;
+
+
+				}
+				double time1 = omp_get_wtime();
+				double megaTrialsPerSecond = (double)NUMTRIALS / (time1 - time0) / 1000000.;
+				if (megaTrialsPerSecond > maxPerformance)
+					maxPerformance = megaTrialsPerSecond;
+				currentProb = (float)numHits / (float)NUMTRIALS;
+
+
+			}
+			printf("%d\t%d\t%5.2lf\t%5.2lf\n", i, NUMTRIALS, currentProb, maxPerformance);
+			/*
+			printf("The Total Threads is %d\n", NUMT);
+			printf("The number of Trials is %d\n", NUMTRIALS);
+			printf("The Probability of Hit hte plate is %5.2lf\n", currentProb);
+			printf("The Max performance is %5.2lf\n", maxPerformance);
+			*/
+#pragma omp barrier
 		}
-		double time1 = omp_get_wtime();
-		double megaTrialsPerSecond = (double)NUMTRIALS / (time1 - time0) / 1000000.;
-		if (megaTrialsPerSecond > maxPerformance)
-			maxPerformance = megaTrialsPerSecond;
-		currentProb = (float)numHits / (float)NUMTRIALS;
-
-
 	}
-	printf("The Total Threads is %d\n", NUMT);
-	printf("The number of Trials is %d\n", NUMTRIALS);
-	printf("The Probability of Hit hte plate is %5.2lf\n", currentProb);
-	printf("The Max performance is %5.2lf\n", maxPerformance);
-
 
 }
 
